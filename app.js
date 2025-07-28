508776a3df37ee9b07b95fedb196d7b760594210
manav parihar <manavparihar2000@gmail.com>
2025-07-28 22:27:15 +0530

    added server side validations

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const listing = require("./models/listing");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const path = require("path");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressErrors = require("./utils/expressErrors.js");
+const { listingschema } = require("./schema.js");
+
 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
@@ -27,8 +31,20 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/Travelbase');
 }
 
+const validateListing = (req, res, next) => {
+    let { error } = listingschema.validate(req.body);
+    if (error) {
+        let errmsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressErrors(400, errmsg)
+    }
+    else {
+        next();
+    }
+
+}
+
 //index route
-app.get("/listings", async (req, res) => {
+app.get("/listings", async (req, res, next) => {
     let allList = await listing.find({});
     res.render("listings/listing.ejs", { allList });
 });
@@ -38,37 +54,48 @@ app.get("/listings/new", (req, res) => {
     res.render("listings/newList.ejs");
 });
 
-app.put("/listings/new", async (req, res) => {
+app.post("/listings/new", validateListing, wrapAsync(async (req, res, next) => {
+
     const newlisting = new listing(req.body.listing);
     let save = await newlisting.save();
-    console.log(save);
     res.redirect("/listings");
-});
+}));
 
 //single list route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let onelist = await listing.findById(id);
     res.render("listings/list.ejs", { onelist });
-});
+}));
 
 //update route
-app.get("/listings/:id/update", async (req, res) => {
+app.get("/listings/:id/update", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let onelist = await listing.findById(id);
     res.render("listings/update.ejs", { onelist });
-});
+}));
 
-app.put("/listings/:id/update", async (req, res) => {
+app.put("/listings/:id/update", validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect("/listings");
-});
+}));
 
 //delete route 
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     await listing.findByIdAndDelete(req.params.id);
     res.redirect("/listings");
+}));
+
+// app.all( "*", (req, res, next) => {
+//     next(new ExpressError(404, "Page not found!"));
+// });
+
+// server side error validation
+app.use((err, req, res, next) => {
+    let { statusCode = 404, message = "page not found" } = err;
+
+    res.status(statusCode).send(message);
 });
 
 app.listen("8080", () => {
--- models/listing.js
+++ models/listing.js
@@ -20,7 +20,10 @@ const listingSchema = new schema({
     set: (v) =>
       v === "" ? "https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?q=80&w=1267&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
   },},
-  price: Number,
+  price: {
+    type: Number,
+    required: true
+  },
   location: String,
   country: String,
 });
