0b9dead702a8719c21a79cea313f2ce88d681711
manav parihar <manavparihar2000@gmail.com>
2025-08-05 16:30:01 +0530

    resolve

--- app.js
+++ app.js
@@ -46,9 +46,6 @@ app.engine("ejs", ejsmate);
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
     touchAfter: 24 * 3600,
 });
 
